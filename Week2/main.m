1c8fe7acf2b7833b6b9edcc0ab37cb58667134e9
Tom Gurka <tommy.gurka@gmail.com>
2014-06-10 21:27:06 -0700

    Initial Commit

---
+++ Week2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Week2
+//
+//  Created by Tom Gurka on 6/10/14.
+//  Copyright (c) 2014 tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
